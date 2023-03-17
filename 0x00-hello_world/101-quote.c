d04c203f922f291d24c19be3822e096dbdc69426
senyodey <cleericdey15@gmail.com>
2023-03-17 06:36:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main -function
- * Return: 1 (success)
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, sizeof(str) - 1);
+return (1);
 }
