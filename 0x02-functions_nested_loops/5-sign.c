623cbd389b81cad8a77447b2f133e7517c5edf2f
senyodey <cleericdey15@gmail.com>
2023-03-21 20:35:21 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+int print_sign(int n)
+
+/**
+
+* print_sign - prints the sign of a number
+* @n: the number to check
+*
+* Return: 1 if positive, -1 if negative, 0 if zero
+*/
+
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
