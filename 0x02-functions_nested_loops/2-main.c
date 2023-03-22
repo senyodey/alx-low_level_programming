9e4924f29983a24265b3575904acd154539cc8f5
senyodey <cleericdey15@gmail.com>
2023-03-22 13:24:50 -0700

    main.h

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
-i}
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
